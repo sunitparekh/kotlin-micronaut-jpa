a600d3f23cbc4eacbd4e322b44a70711016d30ce
Sunit Parekh <parekh.sunit@gmail.com>
2019-06-19 15:13:53 +0530

    removed unwanted imports

--- src/main/kotlin/com/tw/MovieController.kt
+++ src/main/kotlin/com/tw/MovieController.kt
@@ -1,9 +1,7 @@
 package com.tw
 
-import io.micronaut.http.MediaType
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.Produces
 
 @Controller("/movies")
 class MovieController(private val movies: MoviesRepository) {
